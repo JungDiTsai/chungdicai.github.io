3dc96dc2802aa2251f785b1d7b46dfb2304362df
Jung <jung@intella.co>
2020-05-11 13:23:18 +0800

    update

--- control.js
+++ control.js
@@ -1,5 +1,11 @@
 window.onload = function () {
     let stars = document.getElementsByTagName('li')
+    let j = 0
+    while (j < 100) {
+        let star = document.createElement('li')
+        document.getElementsByTagName('ul')[0].appendChild(star)
+        j++
+    }
     for (let i = 0; i < stars.length; i++) {
         let beCircle = Math.floor(Math.random()*10) + 'px'
         stars[i].style.width = beCircle
